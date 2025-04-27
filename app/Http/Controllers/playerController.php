be06493c079569b4cf791207fe887ce058c81b56
singh-dhanraj <dhanrajsingh15823@gmail.com>
2025-04-27 16:39:04 +0530

    working on image moduel and change design of team page

--- app/Http/Controllers/playerController.php
+++ app/Http/Controllers/playerController.php
@@ -7,16 +7,18 @@ use App\Models\Player;
 
 class playerController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $teamId = $request->team_id;
         $team = Team::findOrFail($teamId);
         $players = Player::where('team_id', $teamId)->get();
-        return view('playerlist',['players'=>$players, 'team' => $team]);
+        return view('playerlist', ['players' => $players, 'team' => $team]);
     }
 
 
 
-    public function saveplayer (Request $request){
+    public function saveplayer(Request $request)
+    {
         // dd($request->all());
         $playerId = $request->team_id;
         $playerName = $request->player_name;
@@ -25,7 +27,7 @@ class playerController extends Controller
 
         $imageName = null;
 
-        if ($playerLogo){
+        if ($playerLogo) {
             $image = $request->player_logo;
             $fileName = $playerLogo->getClientOriginalName();
             $newFileNamen = time() . '.' . $fileName;
@@ -35,34 +37,58 @@ class playerController extends Controller
                 mkdir($folderPath, 0777, true);
             }
 
-            if ($image->move($folderPath, $newFileNamen) ) {
+            if ($image->move($folderPath, $newFileNamen)) {
                 $imageName = $newFileNamen;
             }
         }
 
         $create = Player::create([
             'team_id' => $playerId,
-            'player_name' =>  $playerName ,
+            'player_name' => $playerName,
             'player_logo' => $imageName,
             'Player_role' => $playerRole,
         ]);
 
         if ($create) {
             return back();
-        }else {
-            echo 'something went wrong'; 
-        } 
+        } else {
+            echo 'something went wrong';
+        }
 
     }
 
-    public function update(Request $request){
+    public function update(Request $request)
+    {
         // dd($request->all());
         $student = Player::find($request->player_id);
+        // dd($student);
+        $playerLogo = $request->file('player_logo');
+        $imageName = $student->player_logo;
+
+        if ($playerLogo) {
+            $oldPath = public_path('uploads/player_logo/' . $student->player_logo);
+            $oldimage = file_exists($oldPath);
+
+            if ($oldimage) {
+                unlink($oldPath);
+            }
+
+            $image = $request->player_logo;
+            $fileName = $playerLogo->getClientOriginalName();
+            $newFileNamen = time() . '.' . $fileName;
+            $folderPath = public_path('uploads/player_logo');
+
+            if ($image->move($folderPath, $newFileNamen)) {
+                $imageName = $newFileNamen;
+            }
+        }
 
         // dd($student);
         $student->update([
             'player_name' => $request->player_name,
             'Player_role' => $request->player_role,
+            'player_logo' => $imageName
+
         ]);
 
         return redirect()->back();
@@ -77,5 +103,5 @@ class playerController extends Controller
         return redirect()->back();
     }
 
-   
-};
+}
+;
